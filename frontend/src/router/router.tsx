bb4c3aef7e23db9f028d8ab29db42450cda156bd
angela <angelabarrio2000@gmail.com>
2026-02-10 19:58:23 +0100

    refactor(router): ajusta llamada a la 404 tras cambio de router

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -8,7 +8,7 @@ import { NotFound } from '@pages/NotFound';
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />, 
+    element: <App />,
     children: [
       {
         index: true,    
