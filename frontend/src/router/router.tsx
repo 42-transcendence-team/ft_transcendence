19f2290da1f8b19da0301c2297e212a6eaa03bba
OMEN <david-g-v-1997@hotmail.com>
2026-02-26 17:48:59 +0100

    feat(frontend-routing): definición de rutas públicas (login y register)

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -3,6 +3,7 @@ import App from '../App';
 import { createBrowserRouter } from "react-router-dom";
 import { HomePage } from '@pages/HomePage';
 import { Login } from '@pages/Login';
+import { Register } from '@pages/Register';
 import { NotFound } from '@pages/NotFound';
 
 export const router = createBrowserRouter([
@@ -10,14 +11,23 @@ export const router = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
-      {
-        index: true,    
-        element: <HomePage /> 
-      },
+
+      // -------- RUTAS PÚBLICAS --------
       {
         path: "login",
         element: <Login />
       },
+      {
+        path: "register",
+        element: <Register />
+      },
+
+      // (home por ahora sigue aquí, luego la moveremos)
+      {
+        index: true,
+        element: <HomePage />
+      },
+
       {
         path: "*",
         element: <NotFound />
