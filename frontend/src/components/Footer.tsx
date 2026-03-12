de79eba783dd2ed0878dfbebe84635339d9bf09e
OMEN <david-g-v-1997@hotmail.com>
2026-03-12 05:27:18 +0100

    feat(front): Hipervínculos y logo en el componente del Footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,24 @@
+import logo from "../assets/icons/24_logo.png"
+
 export const Footer = () => {
 	return (
-		<>
-			<p>Esto sera un pie de pagina</p>
-		</>
+		<footer>
+
+			<nav>
+				<ul>
+					<li><a href="/about">About</a></li>
+					<li><a href="/cookies">Cookies</a></li>
+					<li><a href="/faq">F.A.Q.</a></li>
+					<li><a href="/contact">Contact</a></li>
+					<li><a href="/developers">Developers</a></li>
+					<li><a href="/privacy-policy">Privacy Policy</a></li>
+				</ul>
+			</nav>
+
+			<div>
+				<img src={logo} alt="Twenty Four logo" width="80" />
+				<span>Twenty Four</span>
+			</div>
+		</footer>
 	)
 }
