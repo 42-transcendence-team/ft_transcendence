c6538677bc913c9548905b4bd06645f79cf45816
Sameidu <sameidudu@gmail.com>
2026-03-16 10:28:10 +0100

    feat(backend): Actualizadas funciones Modify, Delete y Filter. Contruidas dtos en base a peticion

--- backend/internal/dto/userDto.go
+++ backend/internal/dto/userDto.go
@@ -11,3 +11,16 @@ type UserFilter struct {
 	Limit int `form:"limit"`
 	Page  int `form:"page"`
 }
+
+type UserDelete struct {
+	Id uint `form:"id"`
+}
+
+type UserModify struct {
+	Id      uint   `form:"id"`
+	Login   string `form:"login"`
+	Email   string `form:"email"`
+	Name    string `form:"name"`
+	Surname string `form:"surname"`
+	Role    string `form:"role"`
+}
