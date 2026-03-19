5d735907f0ea37acd63da4b43198d136605c3bf1
OMEN <david-g-v-1997@hotmail.com>
2026-03-19 06:39:11 +0100

    feat(frontend): Regex para la password en componente RegisterForm

--- frontend/src/components/RegisterForm.tsx
+++ frontend/src/components/RegisterForm.tsx
@@ -51,10 +51,11 @@ export const RegisterForm = () => {
 		const usernameRegex = /^[A-Za-z0-9_-]+$/
 		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
 		const nameRegex = /^[A-Za-zÁÉÍÓÚáéíóúÑñÜü\s-]+$/
+		const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{8,64}$/
 		if (!username.trim()) {
 			newErrors.username = "El username es obligatorio."
 		} else if (!usernameRegex.test(username)) {
-			newErrors.username = "Solo se permiten letras, números, guion y guion bajo."
+			newErrors.username = "Solo se permiten letras, números, guion y guion bajwwwo."
 		}
 		if (!email.trim()) {
 			newErrors.email = "El email es obligatorio."
@@ -63,8 +64,9 @@ export const RegisterForm = () => {
 		}
 		if (!password) {
 			newErrors.password = "La contraseña es obligatoria."
-		} else if (password.length < 8) {
-			newErrors.password = "La contraseña debe tener al menos 8 caracteres."
+		} else if (!passwordRegex.test(password)) {
+			newErrors.password =
+				"La contraseña debe tener entre 8 y 64 caracteres, incluir una mayúscula, un número y un símbolo como mínimo."
 		}
 		if (!confirmPassword) {
 			newErrors.confirmPassword = "Debes confirmar la contraseña."
