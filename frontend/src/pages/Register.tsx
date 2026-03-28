e3e4e3281710897f7961d8d4f9c9fbe5c2250a5f
angela <angelabarrio2000@gmail.com>
2026-03-28 16:23:50 +0100

    Revert "Merge remote-tracking branch 'origin/front-components' into common-componets"
    This reverts commit 66638a0bda8152de23bdc9ab104bdfbdd3398a6f, reversing
    changes made to 863134a6c6e25f6622b3a3f40ab17c9fd4f99121.

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,15 +1,9 @@
-import { RegisterForm } from "../components/RegisterForm";
-
 export const Register = () => {
   return (
     <>
       <h2>REGISTER</h2>
       <h1>Create account</h1>
       <p>Aquí irán los inputs y botones</p>
-	  <p>New in Twenty Four? Create your free account now!</p>
-	  
-	  <RegisterForm />
-	
-	</>
+    </>
   );
 };
