05bda71e83d93dde56fbdcbdf523bfe8b8f31f0f
angela <angelabarrio2000@gmail.com>
2026-03-28 16:30:20 +0100

    feat(footer): lo elimino

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,24 +1,9 @@
-import logo from "../assets/icons/24_logo.png"
+
 
 export const Footer = () => {
 	return (
 		<footer>
 
-			<nav>
-				<ul>
-					<li><a href="/about">About</a></li>
-					<li><a href="/cookies">Cookies</a></li>
-					<li><a href="/faq">F.A.Q.</a></li>
-					<li><a href="/contact">Contact</a></li>
-					<li><a href="/developers">Developers</a></li>
-					<li><a href="/privacy-policy">Privacy Policy</a></li>
-				</ul>
-			</nav>
-
-			<div>
-				<img src={logo} alt="Twenty Four logo" width="80" />
-				<span>Twenty Four</span>
-			</div>
 		</footer>
 	)
 }
