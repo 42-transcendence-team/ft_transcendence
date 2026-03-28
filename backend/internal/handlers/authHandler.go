193bb106a015e1a184a6e5c51c0595a81d1a9acb
angela <angelabarrio2000@gmail.com>
2026-03-28 18:36:17 +0100

    refactor(validation-request): separo a un archivo helpe la validacion de las requets para que puedan ser reutilizadas

--- backend/internal/handlers/authHandler.go
+++ backend/internal/handlers/authHandler.go
@@ -7,7 +7,6 @@ import (
 	"backend/internal/services"
 	"errors"
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
 	"net/http"
 	"time"
 )
@@ -217,38 +216,3 @@ func (h *AuthHandler) setCookie(c *gin.Context, strToken string, exp time.Time) 
 	http.SetCookie(c.Writer, cookie)
 
 }
-
-/*Request validation*/
-// tal vez esto haya que quitarlo de aqui, pero tampoco se dodne iria
-func ValidationBindRequest(c *gin.Context, req interface{}) error {
-
-	err := c.ShouldBindJSON(&req)
-
-	if err != nil {
-		var validationErr validator.ValidationErrors
-		if errors.As(err, &validationErr) {
-			fields := ValidationErrorsToMap(validationErr)
-			return appErr.NewValidation(fields)
-		}
-		return appErr.NewBadRequest("invalid_request_body")
-	}
-
-	return nil
-}
-
-func ValidationErrorsToMap(validationErr validator.ValidationErrors) map[string]string {
-
-	fields := make(map[string]string)
-
-	for _, err := range validationErr {
-
-		field := err.Field()
-		rule := err.Tag()
-
-		fields[field] = rule
-	}
-
-	return fields
-}
-
-/*End of request validation*/
