8d3b6a046c3f60e32837065c64f6c5322e24d943
Sameidu <sameidudu@gmail.com>
2026-03-31 20:00:24 +0200

    feat(2fa): Pagina de settings en router

---
+++ frontend/src/pages/Settings.tsx
@@ -0,0 +1,12 @@
+
+
+export function Settings() {
+	return (
+		<>
+			<h2>SETTINGS</h2>
+			<h1>Settings</h1>
+			<p>Aquí irán los inputs y botones</p>
+			<p>Here you can change your account settings.</p>
+		</>
+	);
+}
