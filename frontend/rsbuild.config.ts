4284535fc4857ae10a2ececaf79bfbd3eba944ed
iviseras <ignacio.viseras@gmail.com>
2026-04-05 14:32:47 +0200

    profile page

--- frontend/rsbuild.config.ts
+++ frontend/rsbuild.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginSass } from "@rsbuild/plugin-sass"
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
@@ -9,7 +15,14 @@ export default defineConfig({
 	},
 	plugins: [
 		pluginReact(),
-		pluginSass()],
+		pluginSass({
+		sassLoaderOptions: (config) => {
+			const variablesPath = path.join(__dirname, 'src/styles/abstracts/_variables.scss');
+			const normalizedPath = variablesPath.replace(/\\/g, '/');
+			config.additionalData = `@use "${normalizedPath}" as *;`;
+		},
+		}),
+	],
 	html: {
 		title: 'transcendence',
 		favicon: './public/favicon.png'
@@ -25,7 +38,7 @@ export default defineConfig({
 			'@fonts': './src/assets/fonts',
 			'@data': './src/assets/data',
 			'@icons': './src/assets/icons',
-			'@reset': './src/styles/App.scss'
+			'@reset': './src/styles/App.scss',
 		}
-	}
+	},
 });
