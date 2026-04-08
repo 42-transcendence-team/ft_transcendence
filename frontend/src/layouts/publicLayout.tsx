5fe9c41c53abf647568554e123b8c51ba114f195
OMEN <david-g-v-1997@hotmail.com>
2026-04-08 23:54:48 +0200

    feat(layout): Ocultación del Header en páginas de Login y Register

--- frontend/src/layouts/publicLayout.tsx
+++ frontend/src/layouts/publicLayout.tsx
@@ -1,14 +1,20 @@
 
 import { Footer } from "@components/Footer";
 import { Header } from "@components/Header";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 export function PublicLayout() {
 	// Layout común para todas las páginas públicas (footer, header...)
 	// Es en las páginas donde se modifica el body dependiendo de que se muestre en estas.
+
+	// Usado para evitar mostrar el contenido del Header en caso de estar en las páginas asignadas
+	const location = useLocation()
+	const hideOnThisPages = ["/login", "/register"]
+	const noHeader = hideOnThisPages.includes(location.pathname)
+	
 	return (
 		<div>
-			<Header />
+			{!noHeader && <Header />}
 			<Outlet />
 			<Footer />
 		</div>
