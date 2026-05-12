deac009c9363c24b90784e19cd94a4a6cae2f729
Sameidu <sameidudu@gmail.com>
2026-05-12 10:35:15 +0200

    hotfix(router-back): Duplicacion de rutas en back

--- backend/internal/routes/authRouter.go
+++ backend/internal/routes/authRouter.go
@@ -6,15 +6,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func AuthRoutes(api *gin.RouterGroup, authHandler *handlers.AuthHandler, userHandler *handlers.UserHandler) {
+func AuthRoutes(api *gin.RouterGroup, authHandler *handlers.AuthHandler) {
 	// Registra un usuario (user-created-> 201, o bad-request-> 400, o no-valid-request-> 422, o conflict-> 409) en formato JSON
 	api.POST("auth/register", authHandler.Register)
 	// Login de usuario validando credenciales (user-login-> 200, o bad-request-> 400, o no-valid-request-> 422, o unauthorized-> 401) en formato JSON
 	api.POST("auth/login", authHandler.Login)
+	// TODO - Esto creo que no deberia ir aqui
 	// Logout de usuario (user-logout-> 200) en formato JSON
-	api.POST("auth/logout", authHandler.Logout)
-	//NOTE - BORRAR EN PROD - Endpoint solo para testing
-	api.GET("/users", userHandler.Filter)
+	// api.POST("auth/logout", authHandler.Logout)
 
 }
 
--- backend/internal/server/router.go
+++ backend/internal/server/router.go
@@ -39,9 +39,6 @@ func (srv *HTTPServer) Router() {
 		routes.AuthRoutes(publicForNoAuth, authHandler)
 	}
 
-	// la dejo publica de momento, hasta que se implementen mas cosas , pero deberia de psara por el middleware de auth
-	routes.UserRoutes(api, userHandler)
-
 	// Esto en realidad no se como poder hacerlo bonito
 	login := api.Group("/2fa")
 	login.Use(middlewares.TwoFAMiddleware(srv.Conf))
