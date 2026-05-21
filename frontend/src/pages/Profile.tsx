e334e136745ea2eac8307c3465ee127e6075db43
Ignacio Viseras <79020050+ignacioviseras@users.noreply.github.com>
2026-05-21 14:56:39 +0200

    Components (#568)
    * init redis add .env
    
    * Container ft_transcendence-redis-1     Error  need fix
    
    * add redis in go (need test)
    
    * fix error (redis nil)
    
    * no funciona el logout ns pq creo q el boton no va (o es culpa mia)
    
    * login add redis add navigate profile
    
    * redireccionamiento del login al profille usando redis y protegiendo las rutas del user, validacion de datos al mostrar el perfil
    
    * delete comment
    
    * delete commnt
    
    * creacion del componente button1 y post
    
    * add redis-port
    
    * fix merge errors
    
    ---------
    
    Co-authored-by: Sameidu <sameidudu@gmail.com>

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -4,6 +4,46 @@ import skullLogo from '../assets/icons/skull_logo.png';
 import photo1 from '../assets/img/choni1.png';
 import photo2 from '../assets/img/choni2.png';
 import photo3 from '../assets/img/choni3.png';
+import {Post} from '../components/Post'
+import {Button1} from '../components/Button1'
+
+//Todo cambiar los datos y q lleguen de la bd este seria un esquima de datos mas o menos
+const postsData = [//testing de datos
+  {
+    id: 1,
+    username: "lore",
+    time: "Ahora mismo",
+    message: " TODO BORRAR",
+    isHighlighted: false
+  },
+  {
+    id: 2,
+    username: "yoni",
+    time: "2 min",
+    message: "Listo pal roneitoooo",
+    images: [photo1, photo2, photo3, photo3],
+    isHighlighted: true
+  },
+  {
+    id: 3,
+    username: "lore",
+    time: "Ahora mismo",
+    message: " TODO BORRAR",
+    isHighlighted: false
+  },{
+    id: 4,
+    username: "lore",
+    time: "Ahora mismo",
+    message: " TODO BORRAR",
+    isHighlighted: false
+  },{
+    id: 5,
+    username: "lore",
+    time: "Ahora mismo",
+    message: " TODO BORRAR",
+    isHighlighted: false
+  },
+];
 
 export const Profile = () => {
   const { user, loading } = useAuth();
@@ -35,10 +75,7 @@ export const Profile = () => {
             {/* Mostramos el nombre real o el login desde Postgres */}
             <h4 className='user-name'>{user.name && user.surname ? `${user.name} ${user.surname}` : user.login}</h4>
           </div>
-
-          <div className='action-buttons'>
-            <button className='share-btn'>Share</button>
-          </div>
+          <Button1 label='Share'></Button1>
         </div>
 
         {/* 3. Sección del Feed */}
@@ -46,46 +83,21 @@ export const Profile = () => {
           {/* El estado también podría venir del objeto 'user' si lo guardas en DB */}
           <p className='status-text'>{user.status || "Sin estado disponible"}</p>
 
-          <div className='feed-posts-container'>
-            
-            <div className='feed-post'>
-              <div className='post-header'>
-                <div className='small-logo'>
-                  <img src={skullLogo} alt="Username logo" />
-                </div>
-                <div className='post-info'>
-                  <span className='username'>{user.login}</span>
-                  <span className='time'>Ahora mismo</span>
-                </div>
-              </div>
-              <p className='post-message'>
-                ID es {user.id} y email es {user.email}.
-                TODO BORRAR LUEGO
-                {/* TODO BORRAR LUEGO */}
-              </p>
-            </div>
-
-            {/* Post estático de ejemplo */}
-            <div className='feed-post highlighted-post'>
-              <div className='post-header'>
-                <div className='small-logo'>
-                  <img src={skullLogo} alt="Username logo" />
-                </div>
-                <div className='post-info'>
-                  <span className='username'>{user.login}</span>
-                  <span className='time'>2 min</span>
-                </div>
-              </div>
-              <p className='post-message'>Listo pal roneitoooo</p>
-              
-              <div className='photo-gallery'>
-                <img src={photo1} alt="Photo 1" />
-                <img src={photo2} alt="Photo 2" />
-                <img src={photo3} alt="Photo 3" />
-                <div className='more-photos'>+5</div>
-              </div>
-            </div>
-
+          <div className='feed-posts-container'>{/*si quremos añadir contenido necesitamos el feed-posts-container */}
+            {postsData.length > 0 ? (
+              postsData.map((post) => (
+                <Post 
+                  key={post.id}
+                  username={post.username}
+                  time={post.time}
+                  message={post.message}
+                  images={post.images}
+                  isHighlighted={post.isHighlighted}
+                />
+              ))
+            ) : (
+              <div className="no-posts">Aún no hay roneos por aquí...</div>
+            )}
           </div>
         </div>
       </div>
