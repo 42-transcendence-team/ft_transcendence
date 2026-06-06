62b9858ce419a6c012c4dc3001497f85876ba9f4
iviseras <ignacio.viseras@gmail.com>
2026-06-06 18:47:12 +0200

    init profile

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,58 @@
+import {Post} from '@components/Post'
+
+const postsData = [//testing de datos
+  {
+	id: 1,
+	username: "lore",
+	time: "Ahora mismo",
+	message: " TODO BORRAR",
+	isHighlighted: false
+  },
+  {
+	id: 2,
+	username: "yoni",
+	time: "2 min",
+	message: "Listo pal roneitoooo",
+	isHighlighted: true
+  },
+  {
+	id: 3,
+	username: "lore",
+	time: "Ahora mismo",
+	message: " TODO BORRAR",
+	isHighlighted: false
+  },{
+	id: 4,
+	username: "lore",
+	time: "Ahora mismo",
+	message: " TODO BORRAR",
+	isHighlighted: false
+  },{
+	id: 5,
+	username: "lore",
+	time: "Ahora mismo",
+	message: " TODO BORRAR",
+	isHighlighted: false
+  },
+];
+
+
+
 export const HomePage = () => {
 	return (
-		<>
-			<h2>HOME</h2>
-			<h1>Rsbuild with React</h1>
-   		   <p>Start building amazing things with Rsbuild.</p>
-		</>
-	)
-}
+    // <div className='page-wrapper'>
+	<div className='profile-feed'>
+		<div className='feed-posts-container'>{/*si quremos añadir contenido necesitamos el feed-posts-container */}
+			{postsData.map((post) => (
+				<Post 
+					key={post.id}
+					username={post.username}
+					time={post.time}
+					message={post.message}
+					isHighlighted={post.isHighlighted}
+				/>
+			))}
+		</div>
+	// </div>
+	);
+};
